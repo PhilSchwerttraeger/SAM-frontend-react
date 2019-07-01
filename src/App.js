6a752b375a95044f4560d60955d0902be4a5de18
Philipp Schwerttraeger <ps@fkyp.de>
2019-07-01 20:47:01 +0200

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Analysis from './components/Analysis';
 import Statistics from './components/Statistics';
 import DataTable from './components/DataTable';
 
-// General styling (depending on Material UI theme)
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
-const state = {
-  "strings": {
-    "titles": {
-      "analysis": "Analysis",
-      "statistics": "Statistics",
-      "datatable": "Data Table"
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      "strings": {
+        "titles": {
+          "analysis": "Analysis",
+          "statistics": "Statistics",
+          "datatable": "Data Table"
+        }
+      }
     }
   }
-}    
 
-export default function App() {
+  styleRoot = () => {
+    return {
+      flexGrow: 1
+    }
+  }
   
-  const classes = useStyles();
+  stylePaper = () => {
+    return {
+      padding: 20
+    }
+  }
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>
-            <Analysis state={state}/>
-          </Paper>
+  styleGridItem = () => {
+    return {
+      border: "solid 1px"
+    }
+  }
+
+  render() {
+    return (
+      <div style={this.styleRoot()}>
+        <Grid container spacing={3}>
+          <Grid item style={this.styleGridItem()} xs={12} sm={6}>
+            <Paper style={this.stylePaper()}>
+              <Analysis state={this.state}/>
+            </Paper>
+          </Grid>
+          <Grid item style={this.styleGridItem()} xs={12} sm={6}>
+            <Paper className="paper">
+              <Statistics state={this.state}/>
+            </Paper>
+          </Grid>
+          <Grid item style={this.styleGridItem()} xs={12} sm={12}>
+            <Paper className="paper">
+              <DataTable state={this.state}/>
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>
-            <Statistics state={state}/>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={12}>
-          <Paper className={classes.paper}>
-            <DataTable state={state}/>
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
+export default App;
