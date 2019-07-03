975399d2eacec485b7d5f78b8154d4378ea04a22
Philipp Schwerttraeger <ps@fkyp.de>
2019-07-03 16:52:31 +0200

    Add Material UI Datatable

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Analysis extends Component {
-    render() {
-        return (
-            <div>
-                <h2>{this.props.state.strings.titles.analysis}</h2>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2>{this.props.state.strings.titles.analysis}</h2>
+      </div>
+    )
+  }
 }
 
 Analysis.propTypes = {
-    state: PropTypes.object.isRequired
+  state: PropTypes.object.isRequired
 }
