e132e42be63f2df6c7f2ef85b3e8212ea60ba4d9
Philipp Schwerttraeger <ps@fkyp.de>
2019-07-08 12:23:06 +0200

    Adjust toolbar buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,9 @@ import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import SettingsIcon from '@material-ui/icons/Settings';
+import InfoIcon from '@material-ui/icons/Info';
+import ViewColumnIcon from '@material-ui/icons/ViewColumn';
 
 export default class NavBar extends Component {
   render() {
@@ -14,31 +15,34 @@ export default class NavBar extends Component {
       <div>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="Open drawer"
-            >
+            <IconButton edge="start" color="inherit">
               <MenuIcon />
             </IconButton>
             <Typography variant="title">
               Simple Account Manager
             </Typography>
-          <div style={{flexGrow: 1}}/>
-          <div style={{display: 'flex'}}>
-            <IconButton aria-label="Show 4 new mails" color="inherit">
-                  <MailIcon />
+            <div style={{flexGrow: 1}}/>
+            <div style={{display: 'flex'}}>
+              <IconButton color="inherit">
+                <ViewColumnIcon />
               </IconButton>
-              <IconButton aria-label="Show 17 new notifications" color="inherit">
-                  <NotificationsIcon />
+              <IconButton color="inherit">
+                <SettingsIcon />
               </IconButton>
+              {/*
               <IconButton
-                edge="end"
-                aria-label="Account of current user"
                 aria-haspopup="true"
                 color="inherit"
               >
                 <AccountCircle />
+              </IconButton>
+              */}
+              <IconButton 
+                edge="end" 
+                aria-haspopup="true" 
+                color="inherit"
+              >
+                <InfoIcon />
               </IconButton>
             </div>
           </Toolbar>
