66a84c0313f9bf03f777d46ee504055743d8c9ce
Philipp Schwerttraeger <ps@fkyp.de>
2019-07-18 15:25:26 +0200

    Remove statistics section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Analysis from './components/Analysis';
-import Statistics from './components/Statistics';
 import DataTable from './components/DataTable';
 import NavBar from './components/NavBar';
 import { DataProvider } from './components/DataContext';
@@ -32,14 +31,9 @@ class App extends React.Component {
         <NavBar />
         <div style={this.styleMain()}>
           <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
               <Paper style={this.stylePaper()}>
                 <Analysis />
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper style={this.stylePaper()}>
-                <Statistics />
               </Paper>
             </Grid>
             <Grid item xs={12}>
