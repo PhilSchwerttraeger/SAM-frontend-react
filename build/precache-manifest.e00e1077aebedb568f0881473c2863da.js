8bda782cb31cf5c44f4c5c4c3f3923933c309e1f
Philipp Schwerttraeger <ps@fkyp.de>
2019-08-09 10:15:59 +0200

    Add build folder (for deployment on netlify)

---
+++ build/precache-manifest.e00e1077aebedb568f0881473c2863da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207a280c56bce7c728893f927a56621e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cca938fa5561e3090c71",
+    "url": "/static/css/2.733cd0f7.chunk.css"
+  },
+  {
+    "revision": "7cb8d328705ceb5c1975",
+    "url": "/static/css/main.2b53d54c.chunk.css"
+  },
+  {
+    "revision": "cca938fa5561e3090c71",
+    "url": "/static/js/2.e765cd18.chunk.js"
+  },
+  {
+    "revision": "7cb8d328705ceb5c1975",
+    "url": "/static/js/main.98121a7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
