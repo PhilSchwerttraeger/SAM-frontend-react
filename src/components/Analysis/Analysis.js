2c555db88d4c575ca617e0829a49bd1a303fbfeb
Philipp Schwerttraeger <ps@fkyp.de>
2019-08-19 12:44:50 +0200

    Run Prettier

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
-import { Consumer } from '../DataContext';
-import Grid from '@material-ui/core/Grid';
-import AnalysisFragment from './AnalysisFragment';
+import React, { Component } from "react";
+import { Consumer } from "../DataContext";
+import Grid from "@material-ui/core/Grid";
+import AnalysisFragment from "./AnalysisFragment";
 //import TextField from '@material-ui/core/TextField';
 
 export default class Analysis extends Component {
-  getAndValidateValues = (state) => {
-    
+  getAndValidateValues = state => {
     let selectedEntries = state.getSelectedEntries();
     let ids = [];
     let values = [];
-    if(selectedEntries){
+    if (selectedEntries) {
       selectedEntries.forEach(element => {
-
-        // If not undefined... 
-        if(
-          !(element.data === undefined)
-          && !(element.data.id === undefined)
-          && !(element.data.value === undefined)
-        )
-        // (Cast strings to number)
-        {
-          if(typeof(element.data.value) == "string"){
+        // If not undefined...
+        if (
+          !(element.data === undefined) &&
+          !(element.data.id === undefined) &&
+          !(element.data.value === undefined)
+        ) {
+          // (Cast strings to number)
+          if (typeof element.data.value == "string") {
             let castedNumber = Number(element.data.value);
-            if(isNaN(castedNumber)){
+            if (isNaN(castedNumber)) {
               return;
             }
             element.data.value = castedNumber;
@@ -31,56 +28,67 @@ export default class Analysis extends Component {
 
           // ... add to list
           ids.push(element.data.id);
-          if(element.data.type === "In"){
+          if (element.data.type === "In") {
             values.push(element.data.value);
-          } else if (element.data.type === "Out"){
+          } else if (element.data.type === "Out") {
             values.push(-element.data.value);
           }
-          
         }
-      }); 
+      });
     }
-    
-    return {ids, values};
-  }
+
+    return { ids, values };
+  };
 
   render() {
-    let xs = 12, sm = 6, md = 6, lg = 3, xl = 3;
+    let xs = 12,
+      sm = 6,
+      md = 6,
+      lg = 3,
+      xl = 3;
 
     return (
       <Consumer>
         {state => {
-          let analysisSections = state.data.generalConfig.analysisSections;
-          if(!analysisSections) analysisSections = [""];
+          let analysisSections = state.data.settings.analysisSections;
+          if (!analysisSections) analysisSections = [""];
           let validatedValues = this.getAndValidateValues(state);
-          
+
           return (
             <div>
               {/*
               <h2>{state.data.strings.titles.analysis}</h2>              
               */}
-                <Grid 
-                  container 
-                  spacing={3}
-                  direction="row"
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  {analysisSections.map(type => {
-                    return (
-                      <Grid key={type} item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-                          <AnalysisFragment 
-                            type={type}
-                            visibleRows={validatedValues}
-                          />
-                      </Grid>
-                    )
-                  })}
-                </Grid>
+              <Grid
+                container
+                spacing={3}
+                direction="row"
+                alignItems="flex-start"
+                justify="flex-start"
+              >
+                {analysisSections.map(type => {
+                  return (
+                    <Grid
+                      key={type}
+                      item
+                      xs={xs}
+                      sm={sm}
+                      md={md}
+                      lg={lg}
+                      xl={xl}
+                    >
+                      <AnalysisFragment
+                        type={type}
+                        visibleRows={validatedValues}
+                      />
+                    </Grid>
+                  );
+                })}
+              </Grid>
             </div>
-          )
+          );
         }}
       </Consumer>
-    )
+    );
   }
 }
