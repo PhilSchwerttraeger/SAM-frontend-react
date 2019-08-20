cda800d5bd784c29c1a72926446817fdaf489a54
Philipp Schwerttraeger <ps@fkyp.de>
2019-08-20 20:04:38 +0200

    Decode token

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import { DataProvider } from "./components/DataContext";
 import { ThemeProvider } from "@material-ui/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 import themeFile from "./style/Theme";
-
 const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FirebaseIdToken;
+if (token) {
+  // decode the token
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 class App extends React.Component {
   render() {
